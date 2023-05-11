c5c95d13660d25aa4000b5e6cf833341983c7d95
DuckyBxy <101004365+DuckyBxy@users.noreply.github.com>
2023-05-11 12:01:36 +0200

    Add files via upload

--- wyszukiwarka/chat.js
+++ wyszukiwarka/chat.js
@@ -9,7 +9,7 @@ var openchat = () => {
 
 }
 
-var localuser=prompt('podaj nazwe uzytkownika');
+var localuser='123'
 
 // var create_channel = () => {
 //     axios.post('https://webwizards.home.pl/jacek/chat-api/', {
@@ -24,19 +24,25 @@ var render_message = (response, i) =>{
         let data = response.data.data[i];
             if(data.user == localuser){
                 return `
-                    <div class="message-sent">
-                        ${data.message}
+                <div class="message=out">
+                    <div class="message-sent" >
+                                ${data.message}
                     </div>
-                    <div class="chat-reaction">
-                    <div class="emoji">🥶</div>
-                    <div class="emoji>💀</div>
-                    <div class="emoji">❤️</div> 
+                    <div class="reaction-sent">${data.reactions.map(e => '<span>'+e+'</span>').join('')}</div>
+                        <div class="reaction-add-sent">
+                        +
+                            <div class="chat-reaction">
+                                <div class="emoji" id=${data.message_id}>🥶</div>
+                                <div class="emoji" id=${data.message_id}>💀</div>
+                                <div class="emoji" id=${data.message_id}>❤️</div> 
+                            </div>
+                        </div>
                     </div>
-                    <div class="reaction-add">+</div>
+                </div>
                 `}
             else {
                 return `
-                <div class="message-recieved">
+                <div class="message-recieved" >
 
                         <div class="name">
                             ${data.user}
@@ -45,39 +51,76 @@ var render_message = (response, i) =>{
                         <div class="message">
                             ${data.message}
                         </div>
+                        <div class="reaction">${data.reactions.map(e => '<span>'+e+'</span>').join('')}</div>
+
+                        <div class="reaction-add">
+                        +
                         <div class="chat-reaction">
-                        <div class="emoji">🥶</div>
-                        <div class="emoji">💀</div>
-                        <div class="emoji">❤️</div> 
+                        <div class="emoji" id=${data.message_id}>🥶</div>
+                        <div class="emoji" id=${data.message_id}>💀</div>
+                        <div class="emoji" id=${data.message_id}>❤️</div> 
                         </div>
-                        <div class="reaction-add">+</div>
-                    </div>`
+                        </div>
+                </div>`
             }
 }
 
-// 🥶 💀❤️
+var show_reactions = () => {
+    document.querySelectorAll('.reaction-add').forEach(occurence => {
+        occurence.addEventListener('click', (e) => {
+            e.target.querySelector('.chat-reaction').setAttribute('class', 'chat-reaction-show')
+            })
+        })
+    document.querySelectorAll('.reaction-add-sent').forEach(occurence => {
+        occurence.addEventListener('click', (e) => {
+            e.target.querySelector('.chat-reaction').setAttribute('class', 'chat-reaction-show')
+            })
+        })
+    }
+
 
 var render_messages = (response, leng) =>{
+
     const msgparent = document.querySelector('.chat-content');
     msgparent.innerHTML = '';
+
     for(let i=0;i<leng;i++){
     msgparent.innerHTML += render_message(response, i);
     }
+
 }
 
 setInterval(function() {
     axios.post('https://webwizards.home.pl/jacek/chat-api/', {
+
         all:true,
         channel:'kanal',
         token:'dupaa'
+
     }).then(function(response) {
+
         var leng = response.data.data.length;
         render_messages(response, leng);
+
+            document.querySelectorAll('.emoji').forEach(occurence => {
+
+                occurence.addEventListener('click', (e) => {
+
+                    var reaction = e.target.textContent
+                    var id = e.target.getAttribute("id")
+
+                    axios.post('https://webwizards.home.pl/jacek/chat-api/', {
+
+                        channel:'kanal',
+                        token:'dupaa',
+                        reaction:reaction,
+                        message_id:id
+
+                    })
+                })
+        })
     }).then(function() {
-        console.log(document.querySelectorAll('.emoji').length);
-            document.querySelectorAll('.emoji')[1].addEventListener('click', (e) => {
-                console.log(e.target.textContent);
-            })
+        show_reactions()
     })
 }, 1000)
 
@@ -86,7 +129,8 @@ var message_send = (msg) => {
         channel:'kanal',
         token:'dupaa',
         message:msg,
-        user:localuser
+        user:localuser,
+        reaction:''
     })
 }
 
